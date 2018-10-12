ab7a8074ec349672ae58856e0117701c0938c65f
Ray Shan <ray@shan.io>
2018-10-12 00:43:49 -0700

    Updates

---
+++ precache-manifest.2f0ec8eccb742e4bcf5c87c30530e6d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a15fd7157962dc18ec46b4e2e154077c",
+    "url": "/startup-equity-calculator/static/media/chart.a15fd715.png"
+  },
+  {
+    "revision": "4c00591ad15eae55054e",
+    "url": "/startup-equity-calculator/static/js/runtime~main.4c00591a.js"
+  },
+  {
+    "revision": "09b9ea22030fc94755d1",
+    "url": "/startup-equity-calculator/static/js/main.09b9ea22.chunk.js"
+  },
+  {
+    "revision": "867a1f0df0dd5c6fafbf",
+    "url": "/startup-equity-calculator/static/js/1.867a1f0d.chunk.js"
+  },
+  {
+    "revision": "09b9ea22030fc94755d1",
+    "url": "/startup-equity-calculator/static/css/main.0650552d.chunk.css"
+  },
+  {
+    "revision": "867a1f0df0dd5c6fafbf",
+    "url": "/startup-equity-calculator/static/css/1.57c4baeb.chunk.css"
+  },
+  {
+    "revision": "c2cd8c62d94e7d3b5b71235e963b6583",
+    "url": "/startup-equity-calculator/index.html"
+  }
+];
